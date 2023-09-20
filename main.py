ec17e4b4b5547e3c24881466eaf32fc5c485a433
ijlm <ivan.laverde.85@gmail.com>
2023-09-19 20:52:45 -0500

    con python

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-#if __name__ == "__main__":
-#    import uvicorn
-#   uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=80)
 
