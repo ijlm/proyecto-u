349ec41a63ed5346a1a83ebe5c95ec4d249164a0
ijlm <ivan.laverde.85@gmail.com>
2023-09-19 20:59:51 -0500

    dsf

--- main.py
+++ main.py
@@ -136,7 +136,8 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80)
+#if __name__ == "__main__":
+#    import uvicorn
+#    uvicorn.run(app, host="0.0.0.0", port=80)
 
+#uvicorn main:app --port 80 --reload 
