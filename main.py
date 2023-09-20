56c2743306660f90f560f0e811625519679156fe
ijlm <ivan.laverde.85@gmail.com>
2023-09-19 21:17:47 -0500

    ultimo comit

--- main.py
+++ main.py
@@ -136,8 +136,6 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-#if __name__ == "__main__":
-#    import uvicorn
-#    uvicorn.run(app, host="0.0.0.0", port=80)
-
-#uvicorn main:app --port 80 --reload 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
