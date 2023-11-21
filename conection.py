3eef322205b159953939b038b9ee086d8de27c0c
Ivan Jose Laverde Mesa <41165667+ijlm@users.noreply.github.com>
2023-11-20 19:32:07 -0500

    entorno variables.py

--- conection.py
+++ conection.py
@@ -1,9 +1,12 @@
 import pymssql
+import os
 
-server = 'sql-server-test-analitica.database.windows.net'
-database = 'consultoria'
-username = 'consultoria'
-password = 'Colombia23*'
+# Lee las variables de entorno
+server = os.environ.get('SQL_SERVER', 'sql-server-test-analitica.database.windows.net')
+database = os.environ.get('SQL_DATABASE', 'consultoria')
+username = os.environ.get('SQL_USERNAME', 'consultoria')
+password = os.environ.get('SQL_PASSWORD', 'Colombia23*')
 
 def conectar_db():
     return pymssql.connect(server=server, user=username, password=password, database=database)
+
